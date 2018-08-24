2d8a89fd3997f9c5e2e9c64306a1cb229bd168f1
rafael <rafael@Jyraia>
2018-08-24 08:05:29 -0300

    Começo do programa, nada esta funcional ainda, problema no random pelo menos no meu notebook.

---
+++ src/jogo/Adicao.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jogo;
+import java.util.concurrent.ThreadLocalRandom;
+public class Adicao {
+    private int operador1;
+    private int operador2;
+    public Adicao(){
+       
+
+    }
+}
